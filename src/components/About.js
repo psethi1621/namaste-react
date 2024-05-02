99ed6ab5c8b4b472424bd8219f307827442dec5d
psethi1621 <psethi1621@gmail.com>
2024-05-02 22:35:53 +0530

    using API in componentDidMount

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,25 @@ import React from "react";
 import UserClass from "./UserClass";
 
 class About extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log("Parent constructor");
+  }
+  
   render () {
+    console.log("Parent render");
     return (
       <div>
         <h1>About</h1>
         <h2>This is about page for our dummy swiggy type app</h2>
-        <UserClass name={"Prince (Class)"} location={"Gurugram Class"}></UserClass>
+        <UserClass />
       </div>
     );
+  }
+
+  componentDidMount() {
+    console.log("Parent componentDidMount");
   }
 }
 
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,25 +4,36 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0,
-            count2: 1,
-        };
-        console.log("Child constructor");
+            userInfo : {
+                name: "Dummy",
+                location: "Default",
+            }
+        }
+        // console.log(this.props.name + ": Child constructor");
 
     }
 
     render () {
-        console.log("Child render");
-        const {name, location} = this.props;
-        const {count} = this.state;
+        const {name, location, login} = this.state.userInfo;
+        // console.log(name+": Child render");
         return <div className="user-card">
-            <h1>Count : {count}</h1>
-            <button onClick={() => {
-                this.setState({
-                    count: this.state.count + 1,
-                })
-            }}>Click me</button>
+            <h1>Name : {name}</h1>
+            <h1>location : {location}</h1>
+            <h1>Username: {login} </h1>
         </div>;
+    }
+
+    async componentDidMount() {
+        // console.log(this.props.name+": Child didMount");
+        const data =  await fetch("https://api.github.com/users/psethi1621");
+        const json = await data.json();
+
+        console.log(json);
+
+        this.setState({
+            userInfo : json,
+        })
+
     }
 }
 

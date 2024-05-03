f1ebf894c15a3aeeac362628cf297a8d90b653a1
psethi1621 <psethi1621@gmail.com>
2024-05-03 23:01:52 +0530

    Fixed cdn url for images and added user image on about us page

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const LOGO_URL =
   "https://www.shutterstock.com/image-vector/food-delivery-label-online-shopping-600w-788158546.jpg";
 export const CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const MENU_URL = 
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.45970&lng=77.02820&restaurantId=";
